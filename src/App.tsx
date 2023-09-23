aa744ade126dae0f0fbef6a6d499177e8f823f81
Kursat <draknes.gamin70@gmail.com>
2023-09-23 21:26:02 +0300

    feat: pernosal input component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import styles from "./App.module.scss";
 import CvSheet from "./components/CVSheet/cv-sheet";
+import PersonalInfoInput from "./components/PersonalInfoInput/personal-info-input";
 
 function App() {
+  const [name, setName] = useState<string>("");
+  const [mail, setMail] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
   return (
     <div className={styles["container"]}>
-      <div className={styles["input-field"]}></div>
+      <div className={styles["input-field"]}>
+        <PersonalInfoInput
+          setAddress={setAddress}
+          setPhone={setPhone}
+          setMail={setMail}
+          setName={setName}
+          name={name}
+          mail={mail}
+          phone={phone}
+          address={address}
+        />
+      </div>
       <div className={styles["sheet-field"]}>
         <CvSheet />
       </div>
---
+++ src/components/PersonalInfoInput/personal-info-input.tsx
@@ -0,0 +1,77 @@
+import styles from "./personal-info-input.module.scss";
+interface PersonalInfoInputProps {
+  name: string;
+  mail: string;
+  phone: string;
+  address: string;
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  setMail: React.Dispatch<React.SetStateAction<string>>;
+  setPhone: React.Dispatch<React.SetStateAction<string>>;
+  setAddress: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function PersonalInfoInput({
+  setName,
+  setMail,
+  setPhone,
+  setAddress,
+  name,
+  mail,
+  phone,
+  address,
+}: PersonalInfoInputProps) {
+  return (
+    <div className={styles["container"]}>
+      <h1>Personal Information</h1>
+      <div className={styles["name-input"]} id={styles["input"]}>
+        <label htmlFor="name">Name</label>
+        <input
+          placeholder="First Name Last Name"
+          value={name}
+          type="text"
+          name="name"
+          id={styles["name"]}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+
+      <div className={styles["email-input"]} id={styles["input"]}>
+        <label htmlFor="mail">Email</label>
+        <input
+          placeholder="example@mail.com"
+          value={mail}
+          type="text"
+          name="mail"
+          id={styles["mail"]}
+          onChange={(e) => setMail(e.target.value)}
+        />
+      </div>
+
+      <div className={styles["phone-input"]} id={styles["input"]}>
+        <label htmlFor="phone">Phone Number</label>
+        <input
+          placeholder="+90 546 123 456"
+          value={phone}
+          type="text"
+          name="phone"
+          id={styles["phone"]}
+          onChange={(e) => setMail(e.target.value)}
+        />
+      </div>
+
+      <div className={styles["address-input"]} id={styles["input"]}>
+        <label htmlFor="address">Address</label>
+        <input
+          placeholder="City/District"
+          value={phone}
+          type="text"
+          name="address"
+          id={styles["address"]}
+          onChange={(e) => setMail(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default PersonalInfoInput;
